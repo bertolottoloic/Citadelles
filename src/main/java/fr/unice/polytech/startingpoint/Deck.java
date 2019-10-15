86122b3a3dab72bc155ff2d27013b1a43446eaea
ANAGONOU Sourou Patrick <sourou-patrick.anagonou@etu.unice.fr>
2019-10-15 15:21:52 +0200

    Ajout de la banque et de méthode dans Deck

--- src/main/java/fr/unice/polytech/startingpoint/Deck.java
+++ src/main/java/fr/unice/polytech/startingpoint/Deck.java
@@ -5,8 +5,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
-import java.util.function.Consumer;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -18,17 +18,15 @@ class Deck{
         try {
             JSONArray infos=new JSONArray(readFile(srcData));
 
-            Consumer <Object> c=e->{
+            
+            infos.forEach(e->{
                 int n=((JSONObject)e).getInt("nb");
 
                 for(int i=0;i<n;i++){
                     list.add(new District(((JSONObject)e).getInt("cost"),((JSONObject)e).getInt("value"),((JSONObject)e).getString("color")));
                 }
 
-            };
-
-            
-            infos.forEach(c);
+            });
             
         } catch (Exception e) {
             //TODO: handle exception
@@ -51,6 +49,87 @@ class Deck{
     public void shuffle(){
         Collections.shuffle(list);
     }
+
+    public District withdraw(){
+
+        if(!list.isEmpty()){
+            return list.remove(0);
+        }
+        return null;
+    }
+
+    /**
+     * @param d
+     *      un district
+            
+        On remet le district d sur le Deck
+        
+     * 
+     */
+    public void putbackOne(District d){
+        list.add(d);
+        this.shuffle();
+    }
+
+    /**
+     * @param c
+     *      une collection de District
+            
+        On remet le district d sur le Deck
+        
+     * 
+     */
+    public void putbackMany(Collection<? extends District> c){
+        list.addAll(c);
+        this.shuffle();
+    }
+
+    /**
+     * @param d
+     *      un district
+            
+
+        @return 
+            Un district
+
+        
+     * 
+     */
+    public District exchangeOne(District d){
+        District tmp=this.withdraw();
+        list.add(d);
+        this.shuffle();
+        return tmp;
+    }
+
+    /**
+     * @param c
+            Une collection de cartes à remettre dans le deck
+
+        @return 
+            Une arraylist de district de même taille que la taille du paramètre
+
+            Si le nombre de Districts à échanger est supérieur au nombre de cartes du deck 
+            aucune action n'est entreprise
+        
+     * 
+     */
+    public ArrayList<District> exchangeMany(Collection<? extends District> c){
+        ArrayList <District> l=new ArrayList<>();
+        if(c.size()>list.size()){
+            for(int i=0;i<c.size();i++){
+                l.add(this.withdraw());
+            }
+            list.addAll(c);
+        }
+        return l;
+    }
+
+    public int lenght(){
+        return list.size();
+    }
+
+
 
     
 }

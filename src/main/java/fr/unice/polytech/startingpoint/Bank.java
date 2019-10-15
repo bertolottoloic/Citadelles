d787d067f07eb0dbb10b10c0439e0d1162ed8338
ANAGONOU Sourou Patrick <sourou-patrick.anagonou@etu.unice.fr>
2019-10-15 15:24:44 +0200

    Ajout de la banque

---
+++ src/main/java/fr/unice/polytech/startingpoint/Bank.java
@@ -0,0 +1,39 @@
+package fr.unice.polytech.startingpoint;
+
+
+class Bank{
+    private final int  NBCOINS=30;
+    private int currNbCoins=30;
+
+    Bank(){
+        
+    }
+
+    public int getCurrNbCoins() {
+        return currNbCoins;
+    }
+
+    
+    public boolean withdraw(int nb){
+
+        if(currNbCoins<nb){
+            return false;
+        }
+        else{
+            currNbCoins-=nb;
+            return true;
+            
+        }
+    }
+
+    public boolean deposit(int nb){
+        if((currNbCoins+nb) <=NBCOINS){
+            currNbCoins+=nb;
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+}

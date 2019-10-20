60c4a9d5363076a1278b6cdf325063586a2b768d
hebabz <heba.bouzidi@etu.unice.fr>
2019-10-20 18:15:12 +0200

    Hand classe héritant d'ArrayList + exhangeHands() faite + mini amélioration dans le Main

--- src/main/java/fr/unice/polytech/startingpoint/Hand.java
+++ src/main/java/fr/unice/polytech/startingpoint/Hand.java
@@ -1,26 +1,18 @@
 package fr.unice.polytech.startingpoint;
 
 import java.util.ArrayList;
-public class Hand{
-	private ArrayList<District> myDistricts;
+public class Hand extends ArrayList<District>{
 	
 	Hand(){
-		myDistricts = new ArrayList<>();
 	}
 	
-	ArrayList<District> getMyDistricts(){
-		return myDistricts;
-	}
-	
-	void addDistrict(District newDis){
-		myDistricts.add(newDis);
-	}
-	void addDistricts(ArrayList<District> districts){
-		myDistricts.addAll(districts);
-	}
-	
-	void removeDistrict(District formerDis){
-		myDistricts.remove(formerDis);
+	/**
+	 * Le district est retiré de la main et retourne dans le deck
+	 * 
+	 * @param District en question
+	 */
+	void remove(District formerDis){
+		this.remove(formerDis);
 		/*Remettre le district dans le deck
 				*/
 		Assets.TheDeck.putbackOne(formerDis);

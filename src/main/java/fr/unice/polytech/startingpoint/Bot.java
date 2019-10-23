a7f0094801c2b509ed921edc46811bb701d17e6a
ClementPoueyto <poueyto.clement@gmail.com>
2019-10-23 16:58:51 +0200

    creation classe Bot #14

---
+++ src/main/java/fr/unice/polytech/startingpoint/Bot.java
@@ -0,0 +1,48 @@
+package fr.unice.polytech.startingpoint;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+class Bot{
+    Random random=new Random();
+
+    Bot(){
+    }
+
+    int whatToPick(){
+        return 0;
+    }
+
+    int whatToDoFirst(){
+        return 0;
+    }
+
+    District whatToBuild(ArrayList<District> hand, int gold){
+
+        return null;
+    }
+
+    int wantToActivate(){
+        return 0;
+    }
+
+    Role whoToKill(){
+        return null;
+    }
+
+
+    District whatToDestroy(){
+        return null;
+    }
+    Player whoseCityToDestroy(){
+        return null;
+    }
+
+    Role whoToSteal(){
+        return null;
+    }
+
+
+
+
+}

86b86c2e014ce6f5a1b55d9378a40b0d31495e41
Heb's <hebabouzidi@hotmail.com>
2019-10-31 15:10:06 +0100

    Tests de la classe Game + delete MainTest #19

--- src/test/java/fr/unice/polytech/startingpoint/game/GameTest.java
+++ src/test/java/fr/unice/polytech/startingpoint/game/GameTest.java
@@ -1,32 +1,48 @@
-/*package fr.unice.polytech.startingpoint;
+package fr.unice.polytech.startingpoint.game;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import fr.unice.polytech.startingpoint.board.Board;
+import fr.unice.polytech.startingpoint.player.Bot;
+import fr.unice.polytech.startingpoint.player.Player;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
-    Player p=new Player(1);
-    Game game = new Game(p);
-    String varToBeInitInSetup;
-
+	Player p1 = new Bot(1);
+    Player p2 = new Bot(2);
+    Player p3 = new Bot(3);
+    Game game = new Game(p1, p2, p3);
+    
     @BeforeEach
     void setUp() {
+    	Board board = new Board();
+    	game.setBoard(board);
+    	
     }
 
     @Test
     void dealGoldsTest() {
-        game.dealGolds(3);
-        assertEquals(7, p.getGold());
+    	game.dealGolds(2);
+    	for(Player p : game.getPlayers()) {
+    		assertEquals(2, p.getGold());
+    	}
     }
-    @Test
-    void dealCountPointsTest() {
-        game.countPoints(p);
-        assertEquals(0, game.getPoints().get(p));
-    }
+    
     @Test
     void dealCardsTest() {
-        game.dealCards(4);
-        assertEquals(8, p.getHand().getMyDistricts().size());
+    	game.dealCards(5);
+    	for(Player p : game.getPlayers()) {
+    		assertEquals(5, p.getHand().size());
+    	}
     }
-}*/
+    
+    @Test
+    void countPointsTest() {    	
+    	game.countPoints(p1);
+    	assertEquals(0, game.getPoints().get(p1));
+    	game.countPoints(p2);
+    	assertEquals(0, game.getPoints().get(p2));    	
+    }    
+}
